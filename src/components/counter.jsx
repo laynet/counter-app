904c1b2dace29de258717f4c672e6af1184fc6c2
laynet <layneingramtaylor@gmail.com>
2019-04-10 17:30:45 -0500

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    console.log("Increment clicked", this);
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+}
+
+export default Counter;
