c0fce929be0c0b49af9d9be004a9418a467b8cf9
laynet <layneingramtaylor@gmail.com>
2019-04-11 15:14:19 -0500

    raising and handling events completed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,7 +6,6 @@ class Counter extends Component {
   };
 
   handleIncrement = e => {
-    console.log(e);
     this.setState({ value: this.state.value + 1 });
   };
 
@@ -19,6 +18,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
